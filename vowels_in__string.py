bbacf064ea402274d6b10bb54481d9f973b81386
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-09-07 22:42:14 +0530

    str=input() s=[] for i in str:     if i in 'aeiouAEIOU':         if i not in s:             s.append(i) print(*s)

--- vowels_in__string.py
+++ vowels_in__string.py
@@ -1,8 +1,7 @@
-n=input()
-v="AEIOUaeiou"
-l=[]
-c=0
-for i in n:
-    if i in v and i not in l:
-        l.append(i)
-print(*l)        
+str=input()
+s=[]
+for i in str:
+    if i in 'aeiouAEIOU':
+        if i not in s:
+            s.append(i)
+print(*s)            

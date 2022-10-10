0141093013b6b2db879585deb1773b9ea31e9416
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-10-10 10:47:54 +0530

    n=int(input()) l=list(map(int,input().split())) k=int(input()) b=[] for i in l:     c=0     if l.count(i)==k:         b.append(i)     if b.count(i)==1:         print(i,end=' ') if b==[]:     print("-1")

---
+++ N_times_repeated_elements.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+b=[]
+for i in l:
+    c=0
+    if l.count(i)==k:
+        b.append(i)
+    if b.count(i)==1:
+        print(i,end=' ')
+if b==[]:
+    print("-1")
+        
